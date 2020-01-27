aabe6b485b724404c85b9e5adddd6f3b0552fad0
Attila Thumfart <atthumfart@stud.mediadesign.de>
2020-01-27 16:16:12 +0100

    Controller Interactions Implemented (Basically a script prefab)

---
+++ Assets/Scripts/ControllerControls/ControllerActions.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerActions : MonoBehaviour
+{
+    private bool IsOnField = false;
+    GameObject CurrentField;
+    GameObject CurrentItem;
+
+    PlayerControls controls; // This is where the Controls and actual Input are saved (via Unity Input System)
+
+    private void Awake()
+    {
+        controls.Gameplay.Interact.performed += ctx => SeedField();
+    }
+
+    /*  private void OnTriggerStay(Collider other)
+      {
+          if (Input.GetKeyDown(KeyCode.E) && other.tag == "Field")
+          {
+              other.GetComponent<FieldManager>().SetIsSeeded(true);
+          }
+      } */
+
+    private void OnTriggerEnter(Collider other)         //checks if the Player touches a Field
+    {
+        if (other.tag == "Field")
+        {
+            IsOnField = true;                           //sets the bool for it to true
+            CurrentField = other.gameObject;            //references the current field; used for SeedField to know which field is seeded
+        }
+    }
+
+    private void OnTriggerExit(Collider other)          //checks if the Player stops touching a field
+    {
+        if (other.tag == "Field")
+        {
+            IsOnField = false;                          //sets the bool for it to false
+            CurrentField = null;                        //dereferences the current field
+        }
+    }
+
+    void SeedField()                                    //Seeds the field and gives over the plants information
+    {
+        if (IsOnField)
+        {
+            CurrentItem = FindObjectOfType<MySamplePlant>().gameObject;  //Later: first item in inventory
+
+            CurrentField.GetComponent<FieldManager>().SetIsSeeded(true);
+            CurrentField.GetComponent<FieldManager>().SetGrowthrates(CurrentItem);
+        }
+    }
+
+    private void OnEnable() // This function enables the controls when the object becomes enabled and active
+    {
+        controls.Gameplay.Enable();
+    }
+
+    private void OnDisable() // This function disables the controls when the object becomes disabled or inactive
+    {
+        controls.Gameplay.Disable();
+    }
+}
--- Assets/Scripts/ControllerControls/ControllerMovement.cs
+++ Assets/Scripts/ControllerControls/ControllerMovement.cs
@@ -7,30 +7,30 @@ public class ControllerMovement : MonoBehaviour
 {
     [SerializeField]
     private float playerSpeed = 5f;  // Playerspeed, editable in the Editor
-    
-    private PlayerControls controls;
-    private Vector2 move;
+
+    private PlayerControls controls; // This is where the Controls and actual Input are saved (via Unity Input System)
+    private Vector2 move;            // Vector to save the JoyStick Inputs X and Y
 
     private void Awake()
     {
         controls = new PlayerControls();
 
-        controls.Gameplay.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
-        controls.Gameplay.Move.canceled += ctx => move = Vector2.zero;
+        controls.Gameplay.Move.performed += ctx => move = ctx.ReadValue<Vector2>();  // When the action defined in controls is performed read and save the values of the Joystick in a Vector2
+        controls.Gameplay.Move.canceled += ctx => move = Vector2.zero;               // When there is no input the vector is 0
     }
 
     private void FixedUpdate()
     {
-        Vector3 movement = new Vector3(move.x * playerSpeed, 0f, move.y * playerSpeed) * Time.deltaTime;
-        transform.Translate(movement, Space.World);
+        Vector3 movement = new Vector3(move.x * playerSpeed, 0f, move.y * playerSpeed) * Time.deltaTime; // X and Y Values of the JoyStick are multiplyed with the Playerspeed
+        transform.Translate(movement, Space.World); // The values are applied to move the player in relation to the world
     }
 
-    private void OnEnable()
+    private void OnEnable() // This function enables the controls when the object becomes enabled and active
     {
         controls.Gameplay.Enable();
     }
 
-    private void OnDisable()
+    private void OnDisable() // This function disables the controls when the object becomes disabled or inactive
     {
         controls.Gameplay.Disable();
     }
